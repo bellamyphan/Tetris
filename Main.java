c5996ef3a7f8ebf1dc68ca309d36113253061db0
bellamyphan <thanhhaii777@gmail.com>
2021-03-30 18:22:16 -0500

    initialized java files

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Main.java: This file will contains the main methods.
+
+public class Main {
+    public static void main(String[] args) {
+        // Create the Tetris frame.
+        System.out.println("Create a new Frame.");
+        new Tetris();
+        
+        System.out.println("The main exit correctly.");
+    }
+}
---
+++ TetrisCv.java
@@ -0,0 +1,9 @@
+// TetrisCs.java: This is the canvas.
+
+import java.awt.*;
+import java.awt.event.*;
+
+// This is the canvas.
+public class TetrisCv extends Canvas {
+    
+}
