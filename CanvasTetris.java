bb00b448ca8774350aefe77d37602908d2b7abd2
bellamyphan <thanhhaii777@gmail.com>
2021-03-31 19:32:55 -0500

    Update

--- CanvasTetris.java
+++ CanvasTetris.java
@@ -16,6 +16,12 @@ public class CanvasTetris extends Canvas {
 	float side;
 	// Hold the unit of the canvas (22 x 22 blocks) based on the side.
 	float unit;
+	// (Starting Screen) "PLAY TETRIS" coordinates.
+	int x1, y1, x2, y2;
+	// (Starting Screen) "HIGH SCORES" coordinates.
+	int x3, y3, x4, y4;
+	// (Starting Screen) "EXIT" coordinates.
+	int x5, y5, x6, y6;
 	
 	// Default constructor.
 	public CanvasTetris() {
@@ -67,15 +73,46 @@ public class CanvasTetris extends Canvas {
 	// Starting Screen.
 	// Goal: Print the menu with those options: Play, HighScores, Quit.
 	public void paintMenu(Graphics g) {
+		// Message to the console.
+		System.out.println("Print the Starting Screen.");
+		
 		// Draw the boundary of this canvas.
 		g.drawRect(xCenter - (int) (11 * unit), yCenter - (int) (11 * unit), (int) (22 * unit), (int) (22 * unit));
-		// Paint the PLAY BOX.
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setStroke(new BasicStroke(2));
-		g.drawRect(xCenter - (int) (7 * unit), yCenter - (int) (9 * unit), (int) (14 * unit), (int) (5 * unit));
+		
+		// Set up the Font and the Size for printing strings.
+		g.setFont(new Font("TimesRoman", Font.BOLD, (int) (2 * unit)));
+		
+		// Paint the PLAY TETRIS BOX and store the coordinates.
+		x1 = xCenter - (int) (7 * unit);
+		y1 = yCenter - (int) (9 * unit);
+		x2 = xCenter - (int) (7 * unit) + (int) (14 * unit);
+		y2 = yCenter - (int) (9 * unit) + (int) (5 * unit);
+		g.drawRect(x1, y1, x2 - x1, y2 - y1);
+		//g.drawRect(xCenter - (int) (7 * unit), yCenter - (int) (9 * unit), (int) (14 * unit), (int) (5 * unit));
+		g.drawString("PLAY TETRIS", (int) (xCenter - 6.5 * unit), (int) (yCenter - 5.7 * unit)); 
+		
 		// Paint the HIGH SCORE BOX.
-		g.drawRect(xCenter - (int) (7 * unit), yCenter - (int) (3 * unit), (int) (14 * unit), (int) (5 * unit));
+		x3 = xCenter - (int) (7 * unit);
+		y3 = yCenter - (int) (3 * unit);
+		x4 = xCenter - (int) (7 * unit) + (int) (14 * unit);
+		y4 = yCenter - (int) (3 * unit) + (int) (5 * unit);
+		g.drawRect(x3, y3, x4 - x3, y4 - y3);
+		//g.drawRect(xCenter - (int) (7 * unit), yCenter - (int) (3 * unit), (int) (14 * unit), (int) (5 * unit));
+		g.drawString("HIGH SCORES", (int) (xCenter - 6.9 * unit), (int) (yCenter + 0.2 * unit)); 
+		
 		// Paint the EXIT BOX.
-		g.drawRect(xCenter - (int) (7 * unit), yCenter + (int) (3 * unit), (int) (14 * unit), (int) (5 * unit)); 
+		x5 = xCenter - (int) (7 * unit);
+		y5 = yCenter + (int) (3 * unit);
+		x6 = xCenter - (int) (7 * unit) + (int) (14 * unit);
+		y6 = yCenter + (int) (3 * unit) + (int) (5 * unit);
+		g.drawRect(x5, y5, x6 - x5, y6 - y5);
+		//g.drawRect(xCenter - (int) (7 * unit), yCenter + (int) (3 * unit), (int) (14 * unit), (int) (5 * unit));
+		g.drawString("EXIT", (int) (xCenter - 2.5 * unit), (int) (yCenter + 6.2 * unit)); 
+		
+		// Restore the Font and Size setting.
+		g.setFont(new Font("TimesRoman", Font.PLAIN, (int) (20)));
+		
+		/*// Scaling.
+		g.drawLine(xCenter, 0, xCenter, maxY);*/
 	}
 }
